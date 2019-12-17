773706776b8028bcd1bdf69046614a9789dae838
Minifyre <minifyre@gmail.com>
2019-12-16 21:01:08 -0600

    state={x:0,y:0}

--- index.js
+++ index.js
@@ -5,44 +5,42 @@ const allEqual=(a,b,...more)=>a===b&&(!more.length||allEqual(b,...more))
 const getCol=(width,height,arr,x)=>[...Array(height)].map((_,i)=>arr[i*width+x])
 const getRow=(width,height,arr,y)=>[...Array(width)].map((_,i)=>arr[y*width+i])
 
-const STATE=
-{
-	grid:
-	[
-		null,null,null,
-		null,null,null,
-		null,null,null
-	]
-}
+const STATE={x:0,o:0}
+
 const resetState=state=>Object.assign(state,JSON.parse(JSON.stringify(STATE)))
 const makeMove=(state,i)=>()=>
 {
-	if(whoWins(state)||state.grid[i]) return//game is over OR non-empty cell
-
-	const turn=whoseTurn(state)
-	state.grid[i]=turn
+	//game is not over AND empty cell
+	if(!whoWins(state)&&!state2grid(state)[i]) state[whoseTurn(state)]+=2**i
 }
 const doesPlayerWin=(state,player)=>
 {
-	const {grid}=state
+	const grid=state2grid(state)
 	const col=x=>getCol(3,3,grid,x)
 	const row=y=>getRow(3,3,grid,y)
 	const equal=line=>allEqual(player,...line)
 
+	// todo: these could be identified by flag sums... (e.g. 7,73,273)
 	return (
 		(equal(row(0)))||(equal(row(1)))||(equal(row(2)))||//rows
 		(equal(col(0)))||(equal(col(1)))||(equal(col(2)))||//cols
 		(equal([grid[0],grid[4],grid[8]]))||(equal([grid[2],grid[4],grid[6]]))//diagonals
 	)
 }
-const whoseTurn=({grid})=>
-	grid.filter(cell=>cell==='o').length<grid.filter(cell=>cell==='x').length
+const state2grid=state=>[...Array(9)].map((_,i)=>
+	state.x&(2**i)?'x':
+	state.o&(2**i)?'o':
+	null
+)
+const whoseTurn=state=>
+	state2grid(state).filter(cell=>cell==='o').length<state2grid(state).filter(cell=>cell==='x').length
 	?'o'
 	:'x'
 const whoWins=state=>
 	doesPlayerWin(state,'x')?'x':
 	doesPlayerWin(state,'o')?'o':
 	null;
+
 const output=state=>
 {
 	const winner=whoWins(state)
@@ -55,7 +53,7 @@ const output=state=>
 		v.button({on:{click:()=>resetState(state)}},resetLabel)
 	)
 	const board=v.div({class:'board'},
-		...state.grid.map(
+		...state2grid(state).map(
 			(cell,i)=>v.div({class:'cell',on:{click:makeMove(state,i)}},cell||'')
 		)
 	)
